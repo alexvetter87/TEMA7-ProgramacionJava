770df4c4365b9dc4cace47e8d2eab30915ddf164
alexvetter87 <124079579+alexvetter87@users.noreply.github.com>
2023-03-31 16:34:02 +0200

    Add files via upload

---
+++ Ejercicio7_01/src/CuentaCorriente.java
@@ -0,0 +1,38 @@
+public class CuentaCorriente {
+
+    String dni;
+    String nombre;
+    double saldo;
+
+    CuentaCorriente(String dni, String nombre) {
+        this.dni = dni;
+        this.nombre = nombre;
+        saldo = 0;
+    }
+
+    boolean egreso(double cant) {
+        boolean operacionPosible;
+        if (saldo >= cant) {
+            saldo -= cant;
+            operacionPosible = true;
+        } else {
+            System.out.println("no hay suficiente");
+            operacionPosible = false;
+        }
+        return (operacionPosible);
+    }
+
+    double ingreso(double cant) {
+        saldo += cant;
+        System.out.println("Tu saldo es: " + saldo);
+        return cant;
+    }
+
+    void mostrarInformacion() {
+        System.out.println("Nombre:" + nombre  );
+        System.out.println("Dni: " + dni);
+        System.out.println("Saldo:" + saldo + "euros");
+
+    }
+
+}
